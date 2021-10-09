5af88cbf1d41761fbb64040f81c51eaf8f77300d
Bravenrl <bravenrl@gmail.com>
2021-10-09 13:47:23 +0300

    add Link to root

--- project/src/components/not-found-page/not-found-paje.tsx
+++ project/src/components/not-found-page/not-found-paje.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import HeaderLogo from '../header-logo/header-logo';
 
 function NotFoundPage () : JSX.Element {
@@ -14,7 +16,7 @@ function NotFoundPage () : JSX.Element {
       <main className="page__main">
         <section style = {{textAlign: 'center'}} >
           <h1 style = {{fontSize: '32px', fontWeight:700, fontStyle:'oblique'}}>404. Page not found</h1>
-          <a  href="/" style={{fontSize: '22px', fontWeight:500, fontStyle:'oblique', color: '#4481c3'}}> Вернуться на главную</a>
+          <Link  to={AppRoute.Root} style={{fontSize: '22px', fontWeight:500, fontStyle:'oblique', color: '#4481c3'}}> Вернуться на главную</Link>
         </section>
 
       </main>
