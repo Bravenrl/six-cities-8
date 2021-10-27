54ce19a7b485b9cf0ecf609a974bf308fbc30dc0
Bravenrl <bravenrl@gmail.com>
2021-10-27 12:30:27 +0300

    add review adapter

--- project/src/services/adapter.ts
+++ project/src/services/adapter.ts
@@ -1,44 +1,55 @@
 import { HostType, OfferType, ServerHostType, ServerOfferType } from '../types/offer';
+import { AuthInfo, ReviewType, ServerAurhInfo, ServerReviewType } from '../types/review';
 
-export const adaptOfferToCient = (offer:ServerOfferType) : OfferType => {
-  const {'is_pro': isPro, 'avatar_url': avatarUrl, ...restHost} = offer.host;
-
-  const adaptedHost: HostType = {
-    ...restHost,
+const adaptHostToClient = (host:ServerHostType): HostType => {
+  const {'is_pro': isPro, 'avatar_url': avatarUrl, ...restUser} = host;
+  return {
+    ...restUser,
     isPro,
     avatarUrl,
   };
+};
 
+export const adaptOfferToCient = (offer:ServerOfferType) : OfferType => {
   const {'is_favorite': isFavorite, 'is_premium': isPremium, 'max_adults': maxAdults,
     'preview_image': previewImage, host, ...rest}=offer;
-
-  const adaptedOffer = {
+  return {
     ...rest,
     isFavorite,
     isPremium,
     maxAdults,
     previewImage,
-    host: adaptedHost,
+    host: adaptHostToClient(host),
   };
-
-
-  return adaptedOffer;
 };
 
+export const adaptReviewToCient = (review:ServerReviewType) : ReviewType => {
+  const {user, ...rest}=review;
+  return {
+    ...rest,
+    user: adaptHostToClient(user),
+  };
+};
+
+export const adaptAuthInfoToClient = (authInfo: ServerAurhInfo): AuthInfo => {
+  const {email, token, ...rest}=authInfo;
+  return {
+    ...adaptHostToClient(rest),
+    email,
+    token,
+  };
+};
 
 export const adaptOfferToServer = (offer:OfferType) : ServerOfferType => {
   const {isPro, avatarUrl, ...restHost} = offer.host;
-
   const adaptedHost: ServerHostType = {
     ...restHost,
     'is_pro': isPro,
     'avatar_url': avatarUrl,
   };
-
   const {isFavorite, isPremium, maxAdults,
     previewImage, host, ...rest}=offer;
-
-  const adaptedOffer = {
+  return {
     ...rest,
     'is_favorite': isFavorite,
     'is_premium': isPremium,
@@ -46,7 +57,5 @@ export const adaptOfferToServer = (offer:OfferType) : ServerOfferType => {
     'preview_image': previewImage,
     host: adaptedHost,
   };
-  return adaptedOffer;
 };
-
 
--- project/src/types/review.ts
+++ project/src/types/review.ts
@@ -1,9 +1,30 @@
-import { HostType } from './offer';
+import { HostType, ServerHostType } from './offer';
 
-export type ReviewType = {
+export type CommentType = {
   comment: string;
   date: string;
+};
+
+export type ReviewType = CommentType&{
   id: number;
   rating: number;
   user: HostType;
-}
+};
+
+export type ServerReviewType = Omit<ReviewType, 'user'>&{
+  user: ServerHostType;
+};
+
+export type User = {
+  email: string;
+  password: string;
+};
+
+export type Auth = {
+  email: string;
+  token: string;
+};
+
+export type ServerAurhInfo = Auth&ServerHostType;
+
+export type AuthInfo = Auth&HostType;
